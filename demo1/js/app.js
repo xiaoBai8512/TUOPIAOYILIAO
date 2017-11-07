6870ca62bbfaea8e648a0cb45d763b3a54d496f1
xiaoBai8512 <851285142@qq.com>
2017-11-07 14:06:42 +0800

    demo

---
+++ demo1/js/app.js
@@ -0,0 +1,17 @@
+
+for(var i=0;i<3;i++){
+    $(".container").append("<div class=\"m_Grid2\">\n" +
+        "                <div class=\"m_T2\">\n" +
+        "                    <p class=\"p4\"><a href=\"#\"><b>世纪华通手游布局细分市场  盛大游戏《神无月</b></a></p>\n" +
+        "                </div>\n" +
+        "                <div class=\"m_Box1\">\n" +
+        "                    <a href='#'><img src='../images/activity-img/3_102512323c008.jpg' alt='世纪华通手游布局细分市场  盛大游戏《神无月' height='100' width='150' /></a>\n" +
+        "                    App Annie日前发布了9月中国移动游戏公司收入排名，凭借《神无月》等手游的强势表现，盛大游戏的收入排名大幅上升77位，进入榜单前十，成为最受关注的榜单变化公司。 App Annie 9月中国移动游戏公司收入榜单 值得注意的是，由于《热血传奇》、《龙之谷》、《...\n" +
+        "                </div>\n" +
+        "                <div class=\"d1\">\n" +
+        "                    <div class=\"c9\">\n" +
+        "                        <a title=\"世纪华通手游布局细分市场  盛大游戏《神无月\" href=\"#\">阅读全文</a>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>")
+}
---
+++ demo2/images/custom-style.php
@@ -0,0 +1,347 @@
+
+body{
+    background-color: #ffffff;
+    color: #595959;
+    font-size: 15px;
+    font-family: Droid Serif, sans-serif;
+    font-weight: 400;
+    line-height: 28px;
+}
+
+h1, 
+h2, 
+h3, 
+h4, 
+h5, 
+h6{
+    color: #212121;
+    font-family: Poppins, sans-serif;
+    font-weight: 700;
+}
+h1{
+    font-size: 36px;
+    line-height: 48px;
+}
+h2{
+    font-size: 30px;
+    line-height: 36px;
+}
+h3{
+    font-size: 24px;
+    line-height: 30px;
+}
+h4{
+    font-size: 18px;
+    line-height: 20px;
+}
+h5{
+    font-size: 16px;
+    line-height: 18px;
+}
+h6{
+    font-size: 14px;
+    line-height: 16px;
+}
+
+#wrapper{
+    background-color: #ffffff;
+}
+
+.vc_tta.vc_general.vc_tta-style-tab-default .vc_tta-title-text,
+.vc_tta.vc_general.vc_tta-style-tab-border-top .vc_tta-tab>a,
+.nav-tabs>li>a,
+.breaking-news-wrapper .news-prefix-title,
+.basecamp-testimonial blockquote footer,
+.counter-section,
+.widget_mc4wp_form_widget .btn,
+.loadmore-btn,
+.navbar-nav li a,
+.navbar-nav .msm-menu-item .msm-submenu a, 
+.msm-menu-item .respmenu .msm-submenu ul,
+#msm-mobile-menu .respmenu li a{
+    font-family: Poppins;
+}
+
+.header-transparent .navbar-default,
+.navbar-default {
+    ;
+}
+
+.header-transparent .navbar-default .navbar-nav>li>a,
+.navbar-default .navbar-nav>li>a{
+    ;
+}
+
+
+a,
+a:focus{
+    color: #ff3c00;
+}
+
+
+
+a:hover{
+    color: #eb2800;
+}
+
+.backToTop i:hover{
+    background-color: #eb2800;
+}
+
+
+.section-intro h2{
+    color: #212121;
+}
+
+
+
+::selection,
+::-moz-selection,
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color,
+.btn-primary,
+.vc_general.vc_btn3.vc_btn3-color-theme_default_color:hover,
+.vc_general.vc_btn3.vc_btn3-color-theme_default_color:focus,
+.btn-default:hover,
+.btn-default:focus,
+.theme-bg,
+.sction-title-wrapper .separator,
+.header-top-wrapper .social-icon ul li a i:hover,
+.navbar-toggle:hover .icon-bar,
+.navbar-toggle:focus .icon-bar,
+.post.format-link .blog-link a:hover,
+.news-gallery .carousel-control:focus, 
+.news-gallery .carousel-control:hover,
+.pagination>li>a:hover, 
+.pagination>li>span:hover, 
+.pagination>li>a:focus, 
+.pagination>li>span:focus,
+.pagination .current,
+.pagination>.active>a,
+.pagination>.active>span,
+.pagination>.active>a:hover,
+.pagination>.active>span:hover,
+.pagination>.active>a:focus,
+.pagination>.active>span:focus,
+.blog-navigation a:hover,
+.page-pagination a:hover,
+.page-pagination > span,
+.tt-recipe-wrapper .tt-print,
+.widget.widget_mc4wp_form_widget,
+.widget_tag_cloud a:hover,
+.comments-wrapper .comment-form .submit,
+.error-message a,
+.error-message a:hover,
+.footer-section .social-icon a:hover i,
+#toTop:hover,
+.vc_tta-panel.vc_active .vc_tta-panel-heading,
+.navbar-default .navbar-nav .msm-menu-item .msm-submenu .posted-in a,
+#msm-mobile-menu .respmenu .posted-in a,
+.posted-in a,
+.entry-content .more-link:hover,
+.entry-content a.readmore:hover,
+.catagory-dropdown-wrapper ul li a:hover,
+.colored-border,
+.post-share ul li a{
+    background-color: #ff3c00;
+}
+
+
+.theme-bg-color,
+.theme-bg-color a{
+    background-color: #ff3c00 !important;
+}
+
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color:hover,
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color:focus,
+.btn-primary:hover,
+.btn-primary:active,
+.btn-primary:active:focus,
+.btn-primary:focus{
+    background-color: #eb2800;
+}
+
+
+
+
+.theme-color,
+.btn-outline:hover,
+.btn-outline:focus,
+.vc_general.vc_btn3.vc_btn3-color-theme_default_color,
+.btn-default,
+.learnmore-btn:focus,
+.learnmore-btn:hover,
+.check-circle-list li:hover i,
+.header-top-wrapper .contact-info ul li a:hover,
+.temp-change button.checked,
+.header-menu ul li a:hover,
+.breaking-news li a:hover,
+.search-icon.active:after,
+.page-title ul li a:hover,
+.tt-popup i:hover,
+.post-wrapper .featured-wrapper .entry-meta li a:hover,
+.news-wrapper.single-layout-three .entry-meta li a:hover,
+.news-wrapper.single-layout-four .entry-meta li a:hover,
+.news-wrapper.single-layout-five .entry-meta li a:hover,
+.news-wrapper.single-layout-six .entry-meta li a:hover,
+.news-wrapper.single-layout-seven .entry-meta li a:hover,
+.single-post-navigation .entry-title a:hover,
+.tt-popular-post h4 a:hover,
+.widget_recent_entries ul li  a:hover,
+.widget_recent_comments ul li a:hover,
+.widget_archive ul li a:hover,
+.widget_pages ul li a:hover,
+.widget_nav_menu ul li a:hover,
+.widget .entry-meta ul li a:hover,
+.widget_categories ul li a:hover, 
+.widget_meta ul li a:hover,
+.widget_calendar #today,
+.author-links ul li a:hover,
+.author-info h3 a:hover,
+.authors-post a:hover,
+.author-social ul li a:hover,
+.comment-list .pingback a:hover,
+.error-message h2,
+.footer-sidebar .widget_meta ul li a:hover,
+.footer-sidebar ul li i,
+#toTop,
+.post-wrapper.style-one .recent-news .entry-title a:hover,
+.post-wrapper.style-three .recent-news .entry-title a:hover,
+.post-wrapper.style-five .recent-news .entry-title a:hover,
+.post-wrapper.style-six .recent-news .entry-title a:hover,
+.post-wrapper.style-one .recent-news .entry-meta li a:hover,
+.post-wrapper.style-three .recent-news .entry-meta li a:hover,
+.post-wrapper.style-five .recent-news .entry-meta li a:hover,
+.post-wrapper.style-six .recent-news .entry-meta li a:hover,
+.catagory-dropdown-wrapper .category-inline-list ul li.active a,
+.share-selected-text-main-container .share-selected-text-btn:focus, 
+.share-selected-text-main-container .share-selected-text-btn:hover{
+    color: #ff3c00;
+}
+
+
+.widget_recent_entries ul li a:hover,
+.widget_recent_comments ul li a:hover,
+.widget_archive ul li a:hover,
+.widget_pages ul li a:hover,
+.widget_nav_menu ul li a:hover,
+.widget .entry-meta ul li a:hover,
+.footer-sidebar .widget_recent_entries ul li a:hover, 
+.footer-sidebar .widget_recent_comments ul li a:hover, 
+.footer-sidebar .widget_archive ul li a:hover, 
+.footer-sidebar .widget_pages ul li a:hover, 
+.footer-sidebar .widget_nav_menu ul li a:hover,
+.footer-sidebar .widget .entry-meta ul li a:hover,
+.vc_tta-color-theme_default_color.vc_tta-style-outline .vc_tta-panel.vc_active .vc_tta-panel-title>a{
+    color: #ff3c00 !important;
+}
+
+.navbar-default .navbar-nav li.current-menu-ancestor>a,
+.navbar-default .navbar-nav li.current-menu-parent>a,
+.navbar-default .navbar-nav li.current-menu-item>a{
+    color: #ff3c00 !important;
+}
+
+.navbar-default .navbar-nav li a:focus, 
+.navbar-default .navbar-nav li a:hover,
+.navbar-default .navbar-nav>.open>a, 
+.navbar-default .navbar-nav>.open>a:focus, 
+.navbar-default .navbar-nav>.open>a:hover,
+.dropdown-menu>.active>a, 
+.dropdown-menu>.active>a:focus, 
+.dropdown-menu>.active>a:hover,
+.navbar-default .navbar-nav li.current-menu-ancestor.has-mega-menu-child>a:hover,
+.navbar-default .navbar-nav li.current-menu-parent.has-mega-menu-child>a:hover,
+.dropdown-menu>li>a:focus, 
+.dropdown-menu>li>a:hover{
+    color: #ff3c00}
+
+.navbar-default .navbar-nav li.current-menu-ancestor>a, 
+.navbar-default .navbar-nav li.current-menu-parent>a, 
+.navbar-default .navbar-nav li.current-menu-item>a, 
+.respmenu-wrap .respmenu li.current-menu-ancestor>a, 
+.respmenu-wrap .respmenu li.current-menu-parent>a, 
+.respmenu-wrap .respmenu li.current-menu-item>a{
+    color: #ff3c00 !important;
+}
+
+@media(max-width : 767px) {
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:hover,
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:hover,
+    #msm-mobile-menu .respmenu li a:hover,
+    #msm-mobile-menu .respmenu li a:focus{
+        color: #ff3c00    }
+
+    .dropdown-menu-trigger.menu-collapsed{
+        color: #ff3c00;
+    }
+
+    .dropdown-menu-trigger.menu-collapsed,
+    #msm-mobile-menu .respmenu-open:hover hr {
+        border-color: #ff3c00;
+    }
+}
+
+
+
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color,
+.btn-primary,
+.vc_general.vc_btn3.vc_btn3-color-theme_default_color:hover,
+.vc_general.vc_btn3.vc_btn3-color-theme_default_color:focus,
+.btn-default:hover,
+.btn-default:focus,
+.form-control:focus,
+.tt-popup i:hover,
+.single-news-newsletter.subscribe-form .mc4wp-form .form-control:focus,
+.pagination .current,
+.pagination>.active>a,
+.pagination>.active>span,
+.pagination>.active>a:hover,
+.pagination>.active>span:hover,
+.pagination>.active>a:focus,
+.pagination>.active>span:focus,
+.blog-navigation a:hover,
+.widget select:focus,
+.comments-wrapper .comment-form input:focus,
+.comments-wrapper .comment-form textarea:focus,
+.comments-wrapper .comment-form .submit,
+#toTop,
+.vc_tta.vc_general.vc_tta-color-theme_default_color.vc_tta-style-outline .vc_tta-panel.vc_active .vc_tta-panel-heading,
+.featured-video .tt-popup i:hover,
+.catagory-dropdown-wrapper .category-inline-list ul li.active a {
+    border-color: #ff3c00;
+}
+
+
+
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color:hover,
+.vc_general.vc_btn3.vc_btn3-color-theme_primary_color:focus,
+.btn-primary:hover,
+.btn-primary:active,
+.btn-primary:active:focus,
+.btn-primary:focus{
+    border-color: #eb2800;
+}
+
+
+
+.tt-overlay-theme-color,
+.tt-gallery-thumb ul li.flex-active-slide::after{
+    background-color: rgba(255,60,0,.9);
+}
+
+
+
+
+@media (max-width : 767px) {    
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:hover,
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:hover{
+        color: #ff3c00;
+    }
+
+}
