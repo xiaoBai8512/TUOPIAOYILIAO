de1af285b36303e648935c514689c0f776df8003
xiaoBai8512 <851285142@qq.com>
2017-11-14 18:25:48 +0800

    2017.11.14

---
+++ template23/js/tabs.js
@@ -0,0 +1,96 @@
+﻿// 郑州果园工社动画有限公司 2012 - 2013
+//www.31190.com
+
+
+function tabit(btn){
+	var idname = new String(btn.id);
+	var s = idname.indexOf("_");
+	var e = idname.lastIndexOf("_")+1;
+	var tabName = idname.substr(0, s);
+	var id = parseInt(idname.substr(e, 1));
+	var tabNumber = btn.parentNode.childNodes.length;
+	for(i=0;i<tabNumber;i++){
+			//document.getElementById(tabName+"_div_"+i).style.display = "none";
+			document.getElementById(tabName+"_btn_"+i).className = "";
+		};
+		//document.getElementById(tabName+"_div_"+id).style.display = "block";
+		btn.className = "curr";
+};
+
+function etabit(btn){
+	var idname = new String(btn.id);
+	var s = idname.indexOf("_");
+	var e = idname.lastIndexOf("_")+1;
+	var tabName = idname.substr(0, s);
+	var id = parseInt(idname.substr(e, 1));
+	var tabNumber = btn.parentNode.childNodes.length;
+	for(i=0;i<tabNumber;i++){
+			document.getElementById(tabName+"_div_"+i).style.display = "none";
+			document.getElementById(tabName+"_btn_"+i).className = "";
+		};
+		document.getElementById(tabName+"_div_"+id).style.display = "block";
+		btn.className = "curr";
+};
+
+//时间js文件
+tmpDate = new Date();
+date = tmpDate.getDate();
+month= tmpDate.getMonth() + 1 ;
+year= tmpDate.getFullYear();
+document.write(year);
+document.write("年");
+document.write(month);
+document.write("月");
+document.write(date);
+document.write("日 ");
+myArray=new Array(6);
+myArray[0]="星期日"
+myArray[1]="星期一"
+myArray[2]="星期二"
+myArray[3]="星期三"
+myArray[4]="星期四"
+myArray[5]="星期五"
+myArray[6]="星期六"
+weekday=tmpDate.getDay();
+if (weekday==0 | weekday==6)
+{
+document.write(myArray[weekday])
+}
+else
+{document.write(myArray[weekday])
+};
+
+
+// PNG图片透明
+ 
+function correctPNG() // correctly handle PNG transparency in Win IE 5.5 & 6. 
+{ 
+    var arVersion = navigator.appVersion.split("MSIE") 
+    var version = parseFloat(arVersion[1]) 
+    if ((version >= 5.5) && (document.body.filters)) 
+    { 
+       for(var j=0; j<document.images.length; j++) 
+       { 
+          var img = document.images[j] 
+          var imgName = img.src.toUpperCase() 
+          if (imgName.substring(imgName.length-3, imgName.length) == "PNG") 
+          { 
+             var imgID = (img.id) ? "id='" + img.id + "' " : "" 
+             var imgClass = (img.className) ? "class='" + img.className + "' " : "" 
+             var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' " 
+             var imgStyle = "display:inline-block;" + img.style.cssText 
+             if (img.align == "left") imgStyle = "float:left;" + imgStyle 
+             if (img.align == "right") imgStyle = "float:right;" + imgStyle 
+             if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle 
+             var strNewHTML = "<span " + imgID + imgClass + imgTitle 
+             + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";" 
+             + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader" 
+             + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
+             img.outerHTML = strNewHTML 
+             j = j-1 
+          } 
+       } 
+    }    
+} 
+window.attachEvent("onload", correctPNG); 
+
